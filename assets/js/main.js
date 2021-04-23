afac5f2bfd05a31e15321149443372904aa12cf8
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-04-23 13:39:23 +0200

    Set main structure html and basic var in js

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// Ask user age
+var userAge = parseInt(prompt('How old are you?'));
+// Ask how many km
+var userDistance = parseInt(prompt('How long is your journey? Write the Km:'));
+// The price is 0.21$ per km
+var pricePerKm = 0.21;
+// If user age is less than 18 = 20% discount
+
+// If user age is over 65 = 40% discount
+
+// Output the final price in human form
